009e5096abcd20ea0ead6b8178a7abe5c88a5b1e
John Mellor-Crummey <johnmc@rice.edu>
2020-02-03 01:42:28 -0600

    update laghos example

--- examples/gpu/laghos/README.md
+++ examples/gpu/laghos/README.md
@@ -2,3 +2,6 @@ To build laghos, you need to set the following environment variables:
 
 	CUDA_HOME: the top-level directory of a CUDA installation
 	MPI_HOME: the top-level directory of an MPI installation
+
+Your path must contain a version of cmake newer than version 3.3
+Your path must contain a version of gcc newer than version 6.4
--- examples/gpu/laghos/run_hpcrun_long.sh
+++ examples/gpu/laghos/run_hpcrun_long.sh
@@ -4,14 +4,14 @@ export CUPTI_SAMPLING_PERIOD=5
 PC_SAMPLING_DIR=build/cupti_test/cupti-preload/pc_sampling
 LAGHOS_DIR=build/Laghos/cuda
 EXEC=${LAGHOS_DIR}/laghos
-OUT=hpctoolkit-laghos-pc
+OUT=hpctoolkit-laghos-long
 
 # link the executable in this directory for convenience
 rm -f laghos
 ln -s $EXEC
 
 # measure an execution of laghos
-time mpirun -np 4 hpcrun -o $OUT.m -e CPUTIME -e gpu=nvidia -t ${LAGHOS_DIR}/laghos -p 0 -m ${LAGHOS_DIR}/../data/square01_quad.mesh -rs 1 -tf 0.05 -pa
+time mpirun -np 4 hpcrun -o $OUT.m -e CPUTIME -e gpu=nvidia -t ${LAGHOS_DIR}/laghos -p 0 -m ${LAGHOS_DIR}/../data/square01_quad.mesh -rs 2 -tf 0.75 -pa
 
 # compute program structure information for the laghos binary
 echo hpcstruct -j 16 laghos ...
