92d0f0a7bd8672b2b4da24cc7801522c61e88cea
John Mellor-Crummey <johnmc@rice.edu>
2020-08-04 23:35:27 +0000

    draft mpi+openmp support for cooley

---
+++ examples/cpu/mpi+openmp/amg2006/setup-env/cooley.sh
@@ -0,0 +1,5 @@
+export MPI_CC=mpicxx
+export RUN_CMD="qsub -A ATPESC2020 -q ATPESC2020 -t 12 -n 4 job-scripts/run-amg-cray"
+export ANALYZE_CMD="qsub -A ATPESC2020 -q ATPESC2020 -t 10 -n 2 job-scripts/run-profmpi-cray"
+export CLEAN_CMD="/bin/rm -rf *.output *.error *.cobaltlog"
+
