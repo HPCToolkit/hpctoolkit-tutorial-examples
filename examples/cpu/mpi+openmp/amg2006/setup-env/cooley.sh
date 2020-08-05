2280d2bcee0e3eb56a858715338d9aa784ef2b1f
John Mellor-Crummey <johnmc@rice.edu>
2020-08-05 19:30:21 +0000

    adjust amg2006 setup for cooley

--- examples/cpu/mpi+openmp/amg2006/setup-env/cooley.sh
+++ examples/cpu/mpi+openmp/amg2006/setup-env/cooley.sh
@@ -1,7 +1,9 @@
-export MPI_CC=mpicc
-export RUN_CMD="qsub -A ATPESC2020 -q training -t 20 -n 4 job-scripts/run-amg-cooley"
-export ANALYZE_CMD="qsub -A ATPESC2020 -q training -t 10 -n 2 job-scripts/run-profmpi-cooley"
-export CLEAN_CMD="/bin/rm -rf *.output *.error *.cobaltlog"
+echo cooley is not recommended for this tutorial. try theta instead.  
 
-module use /soft/perftools/hpctoolkit/modules
+# export MPI_CC=mpicc
+# export RUN_CMD="qsub -A ATPESC2020 -q training -t 20 -n 4 job-scripts/run-amg-cooley"
+# export ANALYZE_CMD="qsub -A ATPESC2020 -q training -t 10 -n 2 job-scripts/run-profmpi-cooley"
+# export CLEAN_CMD="/bin/rm -rf *.output *.error *.cobaltlog"
+
+# module use /soft/perftools/hpctoolkit/modules
 
