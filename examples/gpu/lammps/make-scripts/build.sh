4d0bd49e4154179181eb6f1da444000790256b8e
Jokeren <robinho364@gmail.com>
2021-03-21 13:36:04 -0700

    Make lampps on cori; still cannot run mpi apps

--- examples/gpu/lammps/make-scripts/build.sh
+++ examples/gpu/lammps/make-scripts/build.sh
@@ -51,6 +51,6 @@ cd build
 git clone https://github.com/lammps/lammps.git
 cd lammps
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPKG_KOKKOS=ON -DPKG_NAME=KOKKOS -DKokkos_ARCH_HOSTARCH=POWER9 -DKokkos_ARCH_GPUARCH=VOLTA70 -DKokkos_ENABLE_CUDA=yes -DKokkos_ENABLE_OPENMP=yes -DCMAKE_CXX_COMPILER=`pwd`/../lib/kokkos/bin/nvcc_wrapper ../cmake
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPKG_KOKKOS=ON -DPKG_NAME=KOKKOS -DKokkos_ARCH_VOLTA70=ON -DKokkos_ARCH_HOSTARCH=POWER9 -DKokkos_ARCH_GPUARCH=VOLTA70 -DKokkos_ENABLE_CUDA=yes -DKokkos_ENABLE_OPENMP=yes -DCMAKE_CXX_COMPILER=`pwd`/../lib/kokkos/bin/nvcc_wrapper ../cmake
 make -j16
 cd ../..
--- examples/gpu/lammps/setup-env/ascent.sh
+++ examples/gpu/lammps/setup-env/ascent.sh
@@ -3,5 +3,5 @@ module load cuda/11.0.2
 module load gcc/6.4.0
 module load cmake/3.17.3
 module load spectrum-mpi
-export HPCTOOLKIT_LAUNCHER=jsrun
+export HPCTOOLKIT_LAUNCHER="jsrun -a 1 -n 4 -g 1"
 
