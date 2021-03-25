638be28d9a77f95200433a2eef0bed0312e484c9
Xiaozhu Meng <mxz297@gmail.com>
2021-03-24 21:26:16 -0400

    Update scripts for Quicksilver

---
+++ examples/gpu/quicksilver/setup-env/summit.sh
@@ -0,0 +1,24 @@
+if [ -z "$HPCTOOLKIT_TUTORIAL_PROJECTID" ]
+then
+  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_PROJECTID to your project ID for submitting jobs"
+elif [ -z "$HPCTOOLKIT_TUTORIAL_RESERVATION" ]
+then
+  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_RESERVATION to hpctoolkit1 for Day 1 (3/29) and hpctoolkit2 for Day 2 (4/2)"
+  echo "if set to default, then the job will be submitted the default batch queue"
+else
+  module purge
+  module load hpctoolkit/2021.03.01
+  module load cuda/11.0.2
+  module load gcc/6.4.0
+  module load cmake/3.17.3
+  module load spectrum-mpi
+
+  unset HPCTOOLKIT_TUTORIAL_GPU_LAMMMPS_READY
+  unset HPCTOOLKIT_TUTORIAL_GPU_MINIQMC_READY
+  unset HPCTOOLKIT_TUTORIAL_GPU_PELEC_READY
+  unset HPCTOOLKIT_TUTORIAL_GPU_LAGHOS_READY
+
+  export HPCTOOLKIT_TUTORIAL_BATCH=1
+  export HPCTOOLKIT_TUTORIAL_GPU_QUICKSILVER_READY=1
+  export HPCTOOLKIT_LAUNCHER_SINGLE_GPU="jsrun -n 1 -g 1 -a 1"
+fi
