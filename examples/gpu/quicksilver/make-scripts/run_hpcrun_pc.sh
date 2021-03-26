312317c96b757905e7911a35913a6966083d9e9b
John Mellor-Crummey <johnmc@rice.edu>
2021-03-26 11:19:20 -0400

    remove ... in quicksilver output

--- examples/gpu/quicksilver/make-scripts/run_hpcrun_pc.sh
+++ examples/gpu/quicksilver/make-scripts/run_hpcrun_pc.sh
@@ -12,24 +12,24 @@ OUT=hpctoolkit-${BINARY}-gpu-cuda-pc
 
 # measure an execution of quicksilver
 RUN="time ${HPCTOOLKIT_QS_LAUNCH} hpcrun -t -o $OUT.m -e gpu=nvidia,pc ${EXEC}"
-echo ${RUN} ...
+echo ${RUN}
 ${RUN}
 
 STRUCT_FILE=${BINARY}-pc.hpcstruct
 
 # compute program structure information for the quicksilver binary
 STRUCT_QS="hpcstruct -j 16 -o $STRUCT_FILE ${EXEC}"
-echo ${STRUCT_QS} ... 
+echo ${STRUCT_QS}
 $STRUCT_QS
 
 # compute program structure information for the quicksilver cubins
 STRUCT_CUBIN="hpcstruct --gpucfg yes $OUT.m" 
-echo ${STRUCT_CUBIN} ... "(note: no \"-j <n>\" for parallel analysis since the cubin is not large)"
+echo ${STRUCT_CUBIN} "(note: no \"-j <n>\" for parallel analysis since the cubin is not large)"
 ${STRUCT_CUBIN}
 
 # combine the measurements with the program structure information
 ANALYZE="hpcprof -S $STRUCT_FILE -o $OUT.d $OUT.m"
-echo $ANALYZE ...
+echo $ANALYZE
 ${ANALYZE}
 
 touch log.run-pc.done
