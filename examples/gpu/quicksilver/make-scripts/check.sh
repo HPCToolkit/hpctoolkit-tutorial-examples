9123aededf5812e4e49db5ff3d0c572409ed4eb9
John Mellor-Crummey <johnmc@rice.edu>
2021-03-26 20:08:45 -0700

    final updates for quicksilver

--- examples/gpu/quicksilver/make-scripts/check.sh
+++ examples/gpu/quicksilver/make-scripts/check.sh
@@ -1,7 +1,7 @@
 if [ -z "$READY" ]
 then
   echo
-  echo "******************************************************************************"
+  echo "*******************************************************************************"
   echo "* Before using make, you must source the setup script for this machine in the *"
   echo "* setup-env subdirectory:                                                     *"
   echo "*     source setup-env/<machine>.sh                                           *" 
