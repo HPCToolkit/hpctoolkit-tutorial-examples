a9a5cd3ced873e00ddefcbcffff5c06f86254e4d
John Mellor-Crummey <johnmc@rice.edu>
2021-03-28 18:57:41 -0400

    overhaul miniqmc on summit

--- examples/gpu/openmp/miniqmc/setup-env/summit.sh
+++ examples/gpu/openmp/miniqmc/setup-env/summit.sh
@@ -1,11 +1,29 @@
 if [ -z "$HPCTOOLKIT_TUTORIAL_PROJECTID" ]
 then
-  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_PROJECTID to your project ID for submitting jobs"
+  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_PROJECTID to your projec
+t id"
+  echo "    'default' to run with your default project id unset"
 elif [ -z "$HPCTOOLKIT_TUTORIAL_RESERVATION" ]
 then
-  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_RESERVATION to hpctoolkit1 for Day 1 (3/29) and hpctoolkit2 for Day 2 (4/2)"
-  echo "if set to default, then the job will be submitted the default batch queue"
+  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_RESERVATION to an approp
+riate value:"
+  echo "    'hpctoolkit1' for day 1"
+  echo "    'hpctoolkit2' for day 2"
+  echo "    'default' to run without the reservation"
 else
+  if test "$HPCTOOLKIT_TUTORIAL_PROJECTID" != "default"
+  then
+    export HPCTOOLKIT_PROJECTID="-P $HPCTOOLKIT_TUTORIAL_PROJECTID"
+  else
+    unset HPCTOOLKIT_PROJECTID
+  fi
+  if test "$HPCTOOLKIT_TUTORIAL_RESERVATION" != "default"
+  then
+    export HPCTOOLKIT_RESERVATION="-U $HPCTOOLKIT_TUTORIAL_RESERVATION"
+  else
+    unset HPCTOOLKIT_RESERVATION
+  fi
+
   module purge
   module load hpctoolkit/2021.03.01
   module load xl
@@ -14,8 +32,12 @@ else
   module load netlib-lapack
   module load cmake/3.17.3
   module load spectrum-mpi
-  export CMAKE_CXX_COMPILER=xlC_r
-  export MINIQMC_COMPILER_FLAGS="-DENABLE_OFFLOAD=1"
+
+  export HPCTOOLKIT_MINIQMC_CXX="xlC_r"
+  export HPCTOOLKIT_MINIQMC_CXXFLAGS="-DENABLE_OFFLOAD=1"
+  export HPCTOOLKIT_MINIQMC_BUILD="sh"
+  export HPCTOOLKIT_MINIQMC_LAUNCH="jsrun -n 1 -g 1 -a 1 -c 11 -brs"
+  export HPCTOOLKIT_MINIQMC_RUN="bsub -P $HPCTOOLKIT_TUTORIAL_PROJECTID -W 5 -nnodes 1 $RESERVE -J miniqmc-run -o log.run.out -e log.run.error $1"
 
   unset HPCTOOLKIT_TUTORIAL_GPU_LAMMMPS_READY
   unset HPCTOOLKIT_TUTORIAL_GPU_LAGHOS_READY
@@ -24,5 +46,4 @@ else
 
   export HPCTOOLKIT_TUTORIAL_BATCH=1
   export HPCTOOLKIT_TUTORIAL_GPU_MINIQMC_READY=1
-  export HPCTOOLKIT_LAUNCHER_SINGLE_GPU="jsrun -n 1 -g 1 -a 1 -c 11 -brs"
 fi
