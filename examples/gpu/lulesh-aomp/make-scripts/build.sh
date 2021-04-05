8bb5aa3fbe01038a2c68ba738a7a3024285d5913
John M Mellor-Crummey <johnmc@ufront.cs.rice.edu>
2021-04-05 13:35:33 -0500

    add aomp version of lulesh to our tutorial examples repository

---
+++ examples/gpu/lulesh-aomp/make-scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+date > .build_begin
+
+$HPCTOOLKIT_LULESH_OMP_MODULES_BUILD
+
+rm -rf LULESH tarfile
+
+mkdir tarfile
+pushd tarfile
+wget http://hpctoolkit.org/sample-codes/lulesh-omp.tgz
+popd
+
+# git clone https://github.com/LLNL/LULESH.git
+# pushd LULESH
+# git checkout 2.0.2-dev
+# pushd omp_4.0
+
+tar xf tarfile/lulesh-omp.tgz
+pushd LULESH/omp_4.0
+echo make CXX="$HPCTOOLKIT_LULESH_OMP_CXX" CXXFLAGS="$HPCTOOLKIT_LULESH_OMP_OMPFLAGS"
+make CXX="$HPCTOOLKIT_LULESH_OMP_CXX" CXXFLAGS="$HPCTOOLKIT_LULESH_OMP_OMPFLAGS"
+popd
+
+touch log.build.done
+date > .build_end
