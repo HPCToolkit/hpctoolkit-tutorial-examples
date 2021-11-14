49d4796160f30e7cb53c852a63a8912036b2d5a1
John Mellor-Crummey <johnmc@rice.edu>
2021-11-14 00:46:11 -0800

    update luleshacc scripts

--- examples/gpu/lulesh-acc/make-scripts/run_hpcrun_pc.sh
+++ examples/gpu/lulesh-acc/make-scripts/run_hpcrun_pc.sh
@@ -6,29 +6,23 @@ $HPCTOOLKIT_MODULES_HPCTOOLKIT
 BINARY=lulesh2.0
 EXEC=LULESH/openacc/src/${BINARY}
 OUT=hpctoolkit-${BINARY}-acc-pc
-STRUCT_FILE=$BINARY.hpcstruct
 
 CMD="rm -rf ${OUT}.m ${OUT}.d $STRUCT_FILE"
 echo $CMD
 $CMD
 
-# measure an execution of quicksilver
-CMD="time ${HPCTOOLKIT_LULESH_ACC_LAUNCH} hpcrun -t -o $OUT.m -e gpu=nvidia,pc ${EXEC} -i 10"
+# measure an execution of lulesh-acc
+CMD="time ${HPCTOOLKIT_LULESH_ACC_LAUNCH} hpcrun -o $OUT.m -e gpu=nvidia,pc ${EXEC} -i 10"
 echo $CMD
 $CMD
 
-# compute program structure information for the quicksilver binary
-CMD="hpcstruct -j 16 -o $STRUCT_FILE ${EXEC}"
-echo $CMD
-$CMD
-
-# compute program structure information for the quicksilver cubins
+# compute program structure information for the lulesh-acc cpu and gpu binaries
 CMD="hpcstruct --gpucfg yes $OUT.m" 
 echo $CMD "(note: no \"-j <n>\" for parallel analysis since the cubin is not large)"
 $CMD
 
 # combine the measurements with the program structure information
-CMD="hpcprof -S $STRUCT_FILE -o $OUT.d $OUT.m"
+CMD="hpcprof -o $OUT.d $OUT.m"
 echo $CMD
 $CMD
 
--- examples/gpu/lulesh-acc/setup-env/cori.sh
+++ examples/gpu/lulesh-acc/setup-env/cori.sh
@@ -1,14 +1,15 @@
+# configure default setup for users
+export HPCTOOLKIT_TUTORIAL_PROJECTID=default
+export HPCTOOLKIT_TUTORIAL_RESERVATION=default
+
 if [ -z "$HPCTOOLKIT_TUTORIAL_PROJECTID" ]
 then
   echo "Please set environment variable HPCTOOLKIT_TUTORIAL_PROJECTID to the apppropriate repository:"
-  echo "    'm3502' for cori users"
   echo "    'ntrain' for training accounts"
   echo "    'default' to run with your default repository, which won't let you use the  reservation"
 elif [ -z "$HPCTOOLKIT_TUTORIAL_RESERVATION" ]
 then
   echo "Please set environment variable HPCTOOLKIT_TUTORIAL_RESERVATION to an appropriate value:"
-  echo "    'hpc1_gpu' for day 1"
-  echo "    'hpc2_gpu' for day 2"
   echo "    'default' to run without the reservation"
 else
   if test "$HPCTOOLKIT_TUTORIAL_PROJECTID" != "default"
@@ -29,31 +30,22 @@ else
   module load cgpu
 
   # load hpctoolkit modules
-  module load hpcviewer/2021.03.01
+  module use /global/common/software/m3977/hpctoolkit/2021-11/modules
+  module load hpctoolkit/2021.11-gpu
 
   # modules for hpctoolkit
-  export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/2021.03.01-gpu"
+  export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/2021.11-gpu"
 
   # environment settings for this example
-  export HPCTOOLKIT_LULESH_ACC_MODULES_BUILD="module load hpcsdk/20.11 cuda/11.0.2"
+  export HPCTOOLKIT_LULESH_ACC_MODULES_BUILD="module load hpcsdk/21.5 cuda/11.3.0"
   export HPCTOOLKIT_LULESH_ACC_CXX="nvc++ -DUSE_MPI=0 -DSEDOV_SYNC_POS_VEL_LATE"
   export HPCTOOLKIT_LULESH_ACC_ACCFLAGS="-acc=gpu -gpu=cc70,lineinfo,rdc -Minfo=accel -fast -gopt"
   export HPCTOOLKIT_LULESH_ACC_SUBMIT="sbatch $HPCTOOLKIT_PROJECTID $HPCTOOLKIT_RESERVATION -N 1 -c 10 -C gpu -t 10"
-  export HPCTOOLKIT_LULESH_ACC_RUN="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-run -o log.run.out -e log.run.error -G 1"
-  export HPCTOOLKIT_LULESH_ACC_RUN_PC="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-run-pc -o log.run-pc.out -e log.run-pc.error -G 1"
-  export HPCTOOLKIT_LULESH_ACC_BUILD="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-build -o log.build.out -e log.build.error"
+  export HPCTOOLKIT_LULESH_ACC_RUN="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-run -o log.run.out -e log.run.stderr -G 1"
+  export HPCTOOLKIT_LULESH_ACC_RUN_PC="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-run-pc -o log.run-pc.out -e log.run-pc.stderr -G 1"
+  export HPCTOOLKIT_LULESH_ACC_BUILD="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-build -o log.build.out -e log.build.stderr"
   export HPCTOOLKIT_LULESH_ACC_LAUNCH="srun -n 1 -G 1"
 
-  # set flag for this example
-  export HPCTOOLKIT_TUTORIAL_GPU_LULESH_ACC_READY=1
-
-  # unset flags for other examples
-  unset HPCTOOLKIT_TUTORIAL_CPU_AMG2013_READY
-  unset HPCTOOLKIT_TUTORIAL_CPU_HPCG_READY
-  unset HPCTOOLKIT_TUTORIAL_GPU_LAGHOS_READY
-  unset HPCTOOLKIT_TUTORIAL_GPU_LAMMPS_READY
-  unset HPCTOOLKIT_TUTORIAL_GPU_LULESH_OMP_READY
-  unset HPCTOOLKIT_TUTORIAL_GPU_MINIQMC_READY
-  unset HPCTOOLKIT_TUTORIAL_GPU_PELEC_READY
-  unset HPCTOOLKIT_TUTORIAL_GPU_QUICKSILVER_READY
+  # mark configuration for this example
+  export HPCTOOLKIT_EXAMPLE=luleshacc
 fi
