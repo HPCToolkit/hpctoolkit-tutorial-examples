22e53eaec7d84a70e6d34cfdebdf7503914d8a40
John Mellor-Crummey <johnmc@rice.edu>
2021-11-13 18:31:51 -0800

    update the scripts for quicksilver

--- examples/gpu/quicksilver/make-scripts/run_hpcrun_pc.sh
+++ examples/gpu/quicksilver/make-scripts/run_hpcrun_pc.sh
@@ -6,7 +6,6 @@ $HPCTOOLKIT_MODULES_HPCTOOLKIT
 BINARY=qs
 EXEC=quicksilver/src/${BINARY}
 OUT=hpctoolkit-${BINARY}-gpu-cuda-pc
-STRUCT_FILE=${BINARY}-pc.hpcstruct
 
 CMD="rm -rf ${OUT}.m ${OUT}.d $STRUCT_FILE"
 echo $CMD
@@ -17,18 +16,13 @@ CMD="time ${HPCTOOLKIT_QS_LAUNCH} hpcrun -o $OUT.m -e gpu=nvidia,pc ${EXEC}"
 echo $CMD
 $CMD
 
-# compute program structure information for the quicksilver binary
-CMD="hpcstruct -j 16 -o $STRUCT_FILE ${EXEC}"
-echo $CMD
-$CMD
-
-# compute program structure information for the quicksilver cubins
+# compute program structure information for the quicksilver cpu and gpu binaries
 CMD="hpcstruct --gpucfg yes $OUT.m" 
 echo $CMD "(note: no \"-j <n>\" for parallel analysis since the cubin is not large)"
 $CMD
 
 # combine the measurements with the program structure information
-CMD="hpcprof -S $STRUCT_FILE -o $OUT.d $OUT.m"
+CMD="hpcprof -o $OUT.d $OUT.m"
 echo $CMD
 $CMD
 
