f62262e57ced7dc6378d15822922361f0577e195
John Mellor-Crummey <johnmc@rice.edu>
2021-11-14 01:30:18 -0800

    update lulesh-omp scripts

--- examples/gpu/lulesh-omp/make-scripts/run_hpcrun_pc.sh
+++ examples/gpu/lulesh-omp/make-scripts/run_hpcrun_pc.sh
@@ -6,9 +6,8 @@ $HPCTOOLKIT_MODULES_HPCTOOLKIT
 BINARY=lulesh2.0
 EXEC=LULESH/omp_4.0/${BINARY}
 OUT=hpctoolkit-${BINARY}-omp-pc
-STRUCT_FILE=$BINARY.hpcstruct
 
-CMD="rm -rf ${OUT}.m ${OUT}.d $STRUCT_FILE"
+CMD="rm -rf ${OUT}.m ${OUT}.d"
 echo $CMD
 $CMD
 
@@ -17,18 +16,13 @@ CMD="time ${HPCTOOLKIT_LULESH_OMP_LAUNCH} hpcrun -t -o $OUT.m -e gpu=nvidia,pc $
 echo $CMD
 $CMD
 
-# compute program structure information for the quicksilver binary
-CMD="hpcstruct -j 16 -o $STRUCT_FILE ${EXEC}"
+# compute program structure information for lulesh-omp cpu and gpu binaries 
+CMD="hpcstruct --gpucfg yes $OUT.m" 
 echo $CMD
 $CMD
 
-# compute program structure information for the quicksilver cubins
-CMD="hpcstruct --gpucfg yes $OUT.m" 
-echo $CMD "(note: no \"-j <n>\" for parallel analysis since the cubin is not large)"
-$CMD
-
 # combine the measurements with the program structure information
-CMD="hpcprof -S $STRUCT_FILE -o $OUT.d $OUT.m"
+CMD="hpcprof -o $OUT.d $OUT.m"
 echo $CMD
 $CMD
 
