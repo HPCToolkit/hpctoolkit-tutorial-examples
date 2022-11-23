6e87595b9a1a87199fb00d9acebf335908fd76f9
Wileam Phan <wphan@vols.utk.edu>
2022-11-23 11:16:03 -0800

    Enable MPI build for Quicksilver GPU on Perlmutter

--- examples/gpu/quicksilver/setup-env/perlmutter.sh
+++ examples/gpu/quicksilver/setup-env/perlmutter.sh
@@ -6,8 +6,6 @@ then
 elif [ -z "$HPCTOOLKIT_TUTORIAL_RESERVATION" ]
 then
   echo "Please set environment variable HPCTOOLKIT_TUTORIAL_RESERVATION to an appropriate value:"
-  echo "    'hpc1_gpu' for day 1"
-  echo "    'hpc2_gpu' for day 2"
   echo "    'default' to run without the reservation"
 else
   if test "$HPCTOOLKIT_TUTORIAL_PROJECTID" != "default"
@@ -25,6 +23,7 @@ else
 
   # cleanse environment
   module purge
+  module load PrgEnv-gnu gpu cmake craype-x86-milan
 
   # load hpctoolkit modules
   module use /global/common/software/m3977/hpctoolkit/2022.10.01/perlmutter/modulefiles
@@ -34,13 +33,14 @@ else
   export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/2022.10.01-gpu"
 
   # environment settings for this example
+  export HPCTOOLKIT_MPI_CXX=CC
   export HPCTOOLKIT_CUDA_ARCH=80
-  export HPCTOOLKIT_QS_MODULES_BUILD="module load cudatoolkit/11.7 cmake gcc"
-  export HPCTOOLKIT_QS_SUBMIT="sbatch $HPCTOOLKIT_PROJECTID $HPCTOOLKIT_RESERVATION -N 1 -c 10 -t 10 -C gpu -q regular"
+  export HPCTOOLKIT_QS_MODULES_BUILD=""
+  export HPCTOOLKIT_QS_SUBMIT="sbatch $HPCTOOLKIT_PROJECTID $HPCTOOLKIT_RESERVATION -N 1 -t 10 -C gpu -q debug"
   export HPCTOOLKIT_QS_RUN="$HPCTOOLKIT_QS_SUBMIT -J qs-run -o log.run.out -e log.run.stderr -G 1"
   export HPCTOOLKIT_QS_RUN_PC="$HPCTOOLKIT_QS_SUBMIT -J qs-run-pc -o log.run-pc.out -e log.run-pc.stderr -G 1"
-  export HPCTOOLKIT_QS_BUILD="$HPCTOOLKIT_QS_SUBMIT -J qs-build -o log.build.out -e log.build.stderr -G 0"
-  export HPCTOOLKIT_QS_LAUNCH="srun -n 1 -G 1"
+  export HPCTOOLKIT_QS_BUILD="sh"
+  export HPCTOOLKIT_QS_LAUNCH="srun -n 4 -c 32 -G 4 --gpus-per-task=1 --gpu-bind=map_gpu:0,1,2,3"
   export HPCTOOLKIT_QS_BEFORE_RUN_PC="srun --ntasks-per-node 1 dcgmi profile --pause"
   export HPCTOOLKIT_QS_AFTER_RUN_PC="srun --ntasks-per-node 1 dcgmi profile --resume"
 
