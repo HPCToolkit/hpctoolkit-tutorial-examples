0c12deadc806fdde1136e5e34bc504450037b681
Wileam Phan <wphan@vols.utk.edu>
2022-12-20 11:50:11 -0500

    Add Perlmutter env script for Laghos

--- examples/gpu/laghos/make-scripts/run_hpcrun_pc.sh
+++ examples/gpu/laghos/make-scripts/run_hpcrun_pc.sh
@@ -15,10 +15,14 @@ CMD="rm -rf $OUT.m $OUT.d"
 echo $CMD
 $CMD
 
+$HPCTOOLKIT_BEFORE_RUN_PC
+
 # measure an execution of laghos
 CMD="time ${HPCTOOLKIT_LAGHOS_LAUNCH} ${HPCTOOLKIT_LAGHOS_LAUNCH_ARGS} hpcrun -o $OUT.m -e gpu=nvidia,pc -t ${LAGHOS_DIR}/laghos -p 0 -dim 2 -rs 1 -tf 0.05 -pa -d cuda"
 echo $CMD
 eval $CMD
+
+$HPCTOOLKIT_AFTER_RUN_PC
 
 # compute program structure information for the laghos cpu and gpu binaries
 CMD="hpcstruct --gpucfg yes $OUT.m"
