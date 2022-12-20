88922aa5704d0e7ed0e54cf3ef15cb4f7920a439
Wileam Phan <wphan@vols.utk.edu>
2022-12-20 11:26:48 -0500

    Update git patch for Laghos

--- examples/gpu/laghos/make-scripts/build.sh
+++ examples/gpu/laghos/make-scripts/build.sh
@@ -14,7 +14,7 @@ git clone https://github.com/CEED/Laghos.git
 pushd Laghos
 git checkout v3.1
 git apply ../../make-scripts/tutorial.patch
-make setup NPROC=16 MFEM_BUILD="pcuda CUDA_ARCH=sm_$HPCTOOLKIT_CUDA_ARCH BASE_FLAGS='-std=c++11 -g'"
+make setup NPROC=16 MFEM_BUILD="${HPCTOOLKIT_LAGHOS_MFEM_FLAGS}"
 make -j 8
 popd
 popd
