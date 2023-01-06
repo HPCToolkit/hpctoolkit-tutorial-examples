3c888e66ac231caa1fbd37eac3a7f7763377308b
Wileam Phan <wphan@vols.utk.edu>
2023-01-06 12:03:02 -0500

    Update crusher env scripts

--- examples/gpu/pelec/setup-env/crusher.sh
+++ examples/gpu/pelec/setup-env/crusher.sh
@@ -32,7 +32,7 @@ else
 
   # modules for hpctoolkit
   module use /gpfs/alpine/csc322/world-shared/modulefiles/x86_64
-  export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit"
+  export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/latest"
   $HPCTOOLKIT_MODULES_HPCTOOLKIT
 
   # environment settings for this example
