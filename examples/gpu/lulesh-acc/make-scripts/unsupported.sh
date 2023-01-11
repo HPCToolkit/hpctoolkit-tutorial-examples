7c9fdd1421834e528678f3f130999a64fbda24df
Wileam Phan <wphan@vols.utk.edu>
2023-01-11 16:47:54 -0500

    Update LULESH-acc build script

--- examples/gpu/lulesh-acc/make-scripts/unsupported.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 
-echo '***********************************************************************************'
-echo '* This experiment type is unsupported on this platform at this time. Some unknown *'
-echo '* interaction with the software stack causes the experiment to hang or fail.      *'
-echo '***********************************************************************************'
-echo 
-exit
