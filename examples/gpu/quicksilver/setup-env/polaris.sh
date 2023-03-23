e8efc211e71b612f9150bc181c2ce582951dbce3
Wileam Phan <wil.phan@rice.edu>
2023-03-23 21:16:52 +0000

    Add Polaris env file for Quicksilver

---
+++ examples/gpu/quicksilver/setup-env/polaris.sh
@@ -0,0 +1,49 @@
+export HPCTOOLKIT_TUTORIAL_RESERVATION=default
+
+if [ -z "$HPCTOOLKIT_TUTORIAL_PROJECTID" ]
+then
+  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_PROJECTID to the apppropriate repository:"
+  echo "    'default' to run with your default repository, which won't let you use the reservation"
+elif [ -z "$HPCTOOLKIT_TUTORIAL_RESERVATION" ]
+then
+  echo "Please set environment variable HPCTOOLKIT_TUTORIAL_RESERVATION to an appropriate value:"
+  echo "    'default' to run without the reservation"
+else
+  if test "$HPCTOOLKIT_TUTORIAL_PROJECTID" != "default"
+  then
+    export HPCTOOLKIT_PROJECTID="-A $HPCTOOLKIT_TUTORIAL_PROJECTID"
+  else
+    unset HPCTOOLKIT_PROJECTID
+  fi
+  if test "$HPCTOOLKIT_TUTORIAL_RESERVATION" != "default"
+  then
+    export HPCTOOLKIT_RESERVATION="-q $HPCTOOLKIT_TUTORIAL_RESERVATION"
+  else
+    export HPCTOOLKIT_RESERVATION="-q debug"
+  fi
+
+  # cleanse environment
+  module purge
+
+  # load modules needed to build and run quicksilver
+  module load PrgEnv-gnu cmake craype-x86-milan cudatoolkit-standalone/12.0.0
+  export CUDA_HOME=/soft/compilers/cudatoolkit/cuda-12.0.0
+
+  # modules for hpctoolkit
+  module use /soft/perftools/hpctoolkit/polaris/modulefiles
+  export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/default"
+  $HPCTOOLKIT_MODULES_HPCTOOLKIT
+
+  # environment settings for this example
+  export HPCTOOLKIT_MPI_CXX=CC
+  export HPCTOOLKIT_CUDA_ARCH=80
+  export HPCTOOLKIT_QS_MODULES_BUILD=""
+  export HPCTOOLKIT_QS_SUBMIT="qsub $HPCTOOLKIT_PROJECTID $HPCTOOLKIT_RESERVATION -l select=1 -l walltime=0:10:00 -l filesystems=home:grand"
+  export HPCTOOLKIT_QS_RUN="$HPCTOOLKIT_QS_SUBMIT -N qs-run -o log.run.out -e log.run.stderr"
+  export HPCTOOLKIT_QS_RUN_PC="$HPCTOOLKIT_QS_SUBMIT -N qs-run-pc -o log.run-pc.out -e log.run-pc.stderr"
+  export HPCTOOLKIT_QS_BUILD="sh"
+  export HPCTOOLKIT_QS_LAUNCH="mpiexec -n 4 -ppn 4 --depth=8 --cpu-bind depth sh make-scripts/set_affinity_gpu_polaris.sh"
+
+  # mark configuration for this example
+  export HPCTOOLKIT_EXAMPLE=quicksilver
+fi
