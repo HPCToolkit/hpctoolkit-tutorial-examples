7e901a55399d541390ee0aebd5735444b094980d
Wileam Phan <wphan@vols.utk.edu>
2023-08-09 09:08:20 -0700

    Hide Perlmutter Laghos env script

--- examples/gpu/laghos/setup-env/.perlmutter.sh
+++ examples/gpu/laghos/setup-env/.perlmutter.sh
@@ -23,8 +23,8 @@ else
   # cleanse environment
   module reset
 
-  # load modules needed to build and run pelec
-  module load gpu PrgEnv-nvidia nvidia/22.7 cray-mpich
+  # load modules needed to build and run Laghos
+  module load gpu PrgEnv-gnu gcc/11.2.0 cudatoolkit/11.7 cray-mpich libfabric craype-x86-milan craype-accel-nvidia80
 
   # modules for hpctoolkit
   export HPCTOOLKIT_MODULES_USE="module use /global/common/software/m3977/modulefiles/perlmutter"
@@ -41,9 +41,9 @@ else
   export HPCTOOLKIT_BEFORE_RUN_PC="srun --ntasks-per-node 1 dcgmi profile --pause"
   export HPCTOOLKIT_AFTER_RUN_PC="srun --ntasks-per-node 1 dcgmi profile --resume"
   export HPCTOOLKIT_LAGHOS_MODULES_BUILD=""
-  export HPCTOOLKIT_LAGHOS_C_COMPILER=nvc
+  export HPCTOOLKIT_LAGHOS_C_COMPILER=gcc
   export HPCTOOLKIT_LAGHOS_MFEM_FLAGS="pcuda CUDA_ARCH=sm_$HPCTOOLKIT_CUDA_ARCH BASE_FLAGS='-std=c++11 -g'"
-  export HPCTOOLKIT_LAGHOS_SUBMIT="sbatch $HPCTOOLKIT_PROJECTID -t 5 -N 1 -C gpu $HPCTOOLKIT_RESERVATION"
+  export HPCTOOLKIT_LAGHOS_SUBMIT="sbatch $HPCTOOLKIT_PROJECTID $HPCTOOLKIT_RESERVATION -t 5 -N 1 -C gpu --export=ALL"
   export HPCTOOLKIT_LAGHOS_RUN_SHORT="$HPCTOOLKIT_LAGHOS_SUBMIT -J laghos-run-short -o log.run-short.out -e log.run-short.error"
   export HPCTOOLKIT_LAGHOS_RUN_LONG="$HPCTOOLKIT_LAGHOS_SUBMIT -J laghos-run-long -o log.run-long.out -e log.run-long.error"
   export HPCTOOLKIT_LAGHOS_RUN_PC="$HPCTOOLKIT_LAGHOS_SUBMIT -J laghos-run-pc -o log.run-pc.out -e log.run-pc.error"
