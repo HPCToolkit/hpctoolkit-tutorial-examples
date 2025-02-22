85508ed6b1ceee928bee42475166a9a6aedbe6ef
John Mellor-Crummey <johnmc@rice.edu>
2025-02-22 21:07:55 +0000

    update aws configuration

--- gpu/nvidia/quicksilver.cuda/setup-env/aws-g4dn.sh
+++ gpu/nvidia/quicksilver.cuda/setup-env/aws-g4dn.sh
@@ -27,19 +27,21 @@ else
     unset HPCTOOLKIT_RESERVATION
   fi
 
-  module load cuda
+  module load cuda/12.2.0-ayvf4gp
+  module load hpctoolkit/2024.01.1-cuda
 
   # modules for hpctoolkit
-  export HPCTOOLKIT_MODULES_HPCTOOLKIT='"module load hpctoolkit/2024.01.1-cuda"'
+  export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/2024.01.1-cuda"
 
   # environment settings for this example
   export HPCTOOLKIT_CUDA_ARCH=75
-  export HPCTOOLKIT_QS_MODULES_BUILD=
+  export HPCTOOLKIT_QS_MODULES_BUILD="source /usr/share/modules/init/sh"
   export HPCTOOLKIT_QS_SUBMIT="qsub -V"
   export HPCTOOLKIT_QS_RUN="$HPCTOOLKIT_QS_SUBMIT -o log.run.out -e log.run.stderr"
   export HPCTOOLKIT_QS_RUN_PC="bash"
   export HPCTOOLKIT_QS_BUILD="bash"
   export HPCTOOLKIT_QS_LAUNCH="mpiexec "
+  export HPCTOOLKIT_QS_ROOT=$PWD
 
   # mark configuration for this example
   export HPCTOOLKIT_EXAMPLE=quicksilver
